fb0d15df0734f5a227d13d19803d834ccd9bd9aa
Cameron Seamons <CameronSeamons@gmail.com>
2024-02-15 22:16:16 -0700

    v 1.1

--- sveltechat/frontend/src/ChatsPage.jsx
+++ sveltechat/frontend/src/ChatsPage.jsx
@@ -1,5 +1,20 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import React from 'react';
+
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+            <div style={{ height: '100vh', overflow: 'hidden' }}>
+            <PrettyChatWindow
+                projectId='2b5451d0-88a8-4ecb-8d7d-ee8f19a0aba4'
+                username= {props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100vh', overflow: 'hidden' }}
+                containerStyle={{ overflow: 'hidden' }}
+            />
+            </div>
+            )
 }
 
 export default ChatsPage
+
