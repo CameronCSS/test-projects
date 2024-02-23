ca955d8c57182102ac284772c4f1d9ec4825a0cc
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2024-02-23 12:46:41 -0700

    Added Lowes

--- willeyTest/puppeteerScript.js
+++ willeyTest/puppeteerScript.js
@@ -4,29 +4,61 @@ import puppeteer from 'puppeteer';
 
 
 const homeDepot = async () => {
-    const url = `https://www.homedepot.com/s/${partNumber}`;
+    // for testing
+    const url = `https://www.homedepot.com/s/WM4000HBA`;
+
+    // const url = `https://www.homedepot.com/s/${partNumber}`;
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     
     await page.goto(url, { timeout: 10000 });
 
-    const pricing = await page.evaluate(() => {
-        const priceElement = document.querySelector('.price');
-        const price = priceElement ? priceElement.textContent.trim() : 'Price not found';
-        return price;
+    const hdPricing = await page.evaluate(() => {
+        const hdPriceElement = document.querySelector('.price');
+        const hdPrice = hdPriceElement ? hdPriceElement.textContent.trim() : 'Price not found';
+        return hdPrice;
     });
 
-    console.log('Home Depot Price:', pricing);
+    console.log('Home Depot Price:', hdPricing);
 
     await browser.close();
 }
 
 homeDepot();
 
+
+const lowes = async () => {
+    // for testing
+    const url = `https://www.lowes.com/search?searchTerm=WM4000HBA`;
+
+
+    // const url = `https://www.lowes.com/search?searchTerm=${partNumber}`;
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36';
+    page.setUserAgent(ua);
+    
+    await page.goto(url, { timeout: 10000 });
+
+    const lowesPricing = await page.evaluate(() => {
+        const lowesPriceElement = document.querySelector('.item-price-dollar');
+        const lowesPrice = lowesPriceElement ? lowesPriceElement.textContent.trim() : 'Price not found';
+        return lowesPrice;
+    });
+
+    console.log('Lowes Price:', lowesPricing);
+
+    await browser.close();
+}
+
+lowes();
+
+
 // Function to delay the execution of the main function until partNumber is available
 const delayMainExecution = () => {
     if (partNumber) {
         homeDepot(); // Call the main function if partNumber is available
+        lowes();
     } else {
         setTimeout(delayMainExecution, 100); // Retry after 100 milliseconds if partNumber is not available yet
     }
